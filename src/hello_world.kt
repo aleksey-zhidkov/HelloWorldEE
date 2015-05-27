45c161504d4107847b0be7257be1ae8b0f16d19f
Aleksey Zhidkov <alexey-zhidkov@yandex.ru>
2015-05-27 22:55:04 +0600

    Name data type added

--- src/hello_world.kt
+++ src/hello_world.kt
@@ -1,8 +1,24 @@
 package helloworld
 
+class Name(val firstName: String,
+           val middleName: String?,
+           val lastName: String?)
+
 class NameFactory {
 
-    public fun createName(): String? = readLine()
+    public fun createName(): Name? = readLine()?.let {
+        val nameParts = it.split(" ")
+        val firstName = nameParts[0]
+
+        val (middleName, lastName) =
+                when (nameParts.size()) {
+                    1 -> Pair(null, null)
+                    2 -> Pair(null, nameParts[1])
+                    else -> Pair(nameParts[1], nameParts[2])
+                }
+
+        Name(firstName, middleName, lastName)
+    }
 
 }
 
